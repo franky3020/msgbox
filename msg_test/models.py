c21937cb5d6f525ea15d191f2a097d47b5cde6fa
franky3020 <24370109franky3020@users.noreply.github.com>
2019-04-11 01:28:28 +0800

    完成分割線_與懸浮按鈕

--- msg_test/models.py
+++ msg_test/models.py
@@ -3,7 +3,7 @@ from django.db import models
 # Create your models here.
 
 class msg_test(models.Model):
-    msg=models.CharField(max_length=100,null=False)
+    msg=models.CharField(max_length=400,null=False)
     
     def __str__(self):
         return self.msg
