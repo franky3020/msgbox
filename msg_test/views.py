d611a4669a91ea54bbc8681337ad395498914d28
franky3020 <24370109+franky3020@users.noreply.github.com>
2019-04-10 19:00:39 +0800

    YA

--- msg_test/views.py
+++ msg_test/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from msg_test.models import msg_test
-
+from django.http import HttpResponseRedirect
 # Create your views here.
 
 
@@ -10,7 +10,15 @@ def msgbox(request):
     
   
     if request.method=="POST":
-        unit=msg_test.objects.create(msg=request.POST['msg'])
-        unit.save()
+        post_msg=request.POST['msg']
+        if(post_msg!=""):
+            unit=msg_test.objects.create(msg=post_msg)
+            unit.save()
+            return HttpResponseRedirect(".")
+            
+            
+        
+        
+        
     return render(request,"msgbox_base.html",locals())
 
